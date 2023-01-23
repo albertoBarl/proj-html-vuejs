c36f12a0453db55ff8e4f194b56adff7e7948de3
albertoBarl <alberto.barletta00@gmail.com>
2023-01-23 22:03:58 +0100

    added "event of the year"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,12 @@
 <template lang="">
+  <!-- event of the year -->
+  <div class="container d-flex justify-content-center gap-4">
+    <span class="text-white"
+      >Starts TOMORROW! Our biggest event of the year...</span
+    >
+    <div></div>
+    <button class="btn">Get ticket</button>
+  </div>
   <div></div>
 </template>
 <script>
@@ -8,4 +16,14 @@ export default {};
 @use "../styles/generals.scss" as *;
 @use "../styles/partials/mixins" as *;
 @use "../styles/partials/variables" as *;
+
+.btn {
+  padding: 5px 40px;
+  background-color: $aranciata;
+  color: $wall;
+  &:hover {
+    background-color: $cielo;
+    color: $wall;
+  }
+}
 </style>
