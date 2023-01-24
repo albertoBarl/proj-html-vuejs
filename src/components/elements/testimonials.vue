790f85ebe026036c2fb0351f9706ab766be87eed
albertoBarl <alberto.barletta00@gmail.com>
2023-01-24 17:45:06 +0100

    fix

--- src/components/elements/testimonials.vue
+++ src/components/elements/testimonials.vue
@@ -7,7 +7,46 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      slider: [
+        {
+          ref: "try",
+          inside:
+            "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+          avatar: "AVATAR",
+          owner: "Owner",
+          job: "Boh",
+        },
+        {
+          ref: "try",
+          inside:
+            "I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.				",
+          avatar: "AVATAR",
+          owner: "Owner",
+          job: "Boh",
+        },
+        {
+          ref: "try",
+          inside:
+            "I am happy with their arrangement of lessons and subjects. They reflect a scientific  investigation into effective methods to be adopted for learners of all levels.				",
+          avatar: "AVATAR",
+          owner: "Owner",
+          job: "Boh",
+        },
+        {
+          ref: "try",
+          inside:
+            "I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.				",
+          avatar: "AVATAR",
+          owner: "Owner",
+          job: "Boh",
+        },
+      ],
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
 @use "../../styles/generals.scss" as *;
