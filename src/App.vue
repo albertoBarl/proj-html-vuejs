5e427979f6b7b56629c2077c9ab9f81475a5ec1d
albertoBarl <alberto.barletta00@gmail.com>
2023-01-25 16:51:30 +0100

    made data w/props

--- src/App.vue
+++ src/App.vue
@@ -85,6 +85,42 @@ export default {
           ],
         },
       ],
+      footerData: [
+        {
+          addressSect: [
+            "382 NE 191st St # 87394 Miami, FL 33179-3899",
+            "+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)",
+            "support@maxcoach.com",
+          ],
+
+          exploreSect: [
+            "Start here",
+            "Success story",
+            "Blog",
+            "Courses",
+            "Contact us",
+          ],
+
+          informationSect: [
+            "Membership",
+            "Purchase Guide",
+            "Privacy policy",
+            "Terms of services",
+          ],
+
+          instaPosts: [
+            "120084500_197897808368764_8114555493043279565_n.jpg",
+            "120012142_177596140500760_8623485824101406058_n.jpg",
+            "120099363_364334431619755_7198812647386067017_n.jpg",
+            "artist-event-01-250x300.jpg",
+            "artist-hero-image-02.jpg",
+            "artist-course-06-480x480.jpg",
+            "artist-hero-image-01.jpg",
+            "artist-hero-image-03.png",
+            "artist-hero-image-04.jpg",
+          ],
+        },
+      ],
     };
   },
 };
@@ -101,7 +137,7 @@ export default {
     <Main />
   </div>
   <div class="footer">
-    <Footer />
+    <Footer :idata="footerData" :key="index" />
   </div>
 </template>
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,20 +7,25 @@
   <!-- proper footer -->
   <div class="container mt-5">
     <!-- row 1 -->
-    <div class="row d-flex">
+    <div class="row d-flex" v-for="(item, index) in idata">
       <!-- address section -->
       <section class="address col-4">
         <h5>Address</h5>
-        <p v-for="(item, index) in addressSect">{{ addressSect[index] }}</p>
+        <p v-for="(item, index) in idata[index].addressSect">
+          {{ item }}
+        </p>
         <socials />
       </section>
 
       <!-- explore section -->
       <section class="col-2">
         <h5>Explore</h5>
-        <ul v-for="(item, index) in exploreSect" class="list-unstyled">
+        <ul
+          v-for="(item, index) in idata[index].exploreSect"
+          class="list-unstyled"
+        >
           <li>
-            <a href="/">{{ exploreSect[index] }}</a>
+            <a href="/">{{ item }}</a>
           </li>
         </ul>
       </section>
@@ -28,9 +33,12 @@
       <!-- information section -->
       <section class="col-2">
         <h5>Information</h5>
-        <ul v-for="(item, index) in informationSect" class="list-unstyled">
+        <ul
+          v-for="(item, index) in idata[index].informationSect"
+          class="list-unstyled"
+        >
           <li>
-            <a href="/">{{ informationSect[index] }}</a>
+            <a href="/">{{ item }}</a>
           </li>
         </ul>
       </section>
@@ -44,13 +52,10 @@
         <div class="d-flex gap-5">
           <div
             class="sPost"
-            v-for="(item, index) in instaPosts"
+            v-for="(item, index) in idata[index].instaPosts"
             :class="[index] > 2 ? 'd-none' : ''"
           >
-            <img
-              :src="`src/assets/images/${instaPosts[index]}`"
-              alt="not available"
-            />
+            <img :src="`src/assets/images/${item}`" alt="not available" />
           </div>
         </div>
       </section>
@@ -75,38 +80,8 @@ export default {
     socials,
     newsletter,
   },
-  data() {
-    return {
-      addressSect: [
-        "382 NE 191st St # 87394 Miami, FL 33179-3899",
-        "+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)",
-        "support@maxcoach.com",
-      ],
-      exploreSect: [
-        "Start here",
-        "Success story",
-        "Blog",
-        "Courses",
-        "Contact us",
-      ],
-      informationSect: [
-        "Membership",
-        "Purchase Guide",
-        "Privacy policy",
-        "Terms of services",
-      ],
-      instaPosts: [
-        "120084500_197897808368764_8114555493043279565_n.jpg",
-        "120012142_177596140500760_8623485824101406058_n.jpg",
-        "120099363_364334431619755_7198812647386067017_n.jpg",
-        "artist-event-01-250x300.jpg",
-        "artist-hero-image-02.jpg",
-        "artist-course-06-480x480.jpg",
-        "artist-hero-image-01.jpg",
-        "artist-hero-image-03.png",
-        "artist-hero-image-04.jpg",
-      ],
-    };
+  props: {
+    idata: Array,
   },
 };
 </script>
